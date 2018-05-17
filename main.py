7beb4101ab875590d76f02f802720cc46dec7305
alexbrisan <s1505883@sms.ed.ac.uk>
2018-05-17 15:26:37 +0100

    Implemented RDP

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
 simple_grammar = '''S
 S -> NP VP
+S -> N
 NP -> Det N
 VP -> eats
 VP -> eates
 Det -> the
 N -> man
+N -> abracadabra
 '''
+
+class Stack:
+    def __init__(self):
+        self.state = []
+
+    def isEmpty(self):
+        return len(self.state) == 0
+
+    def push(self, element):
+        self.state.append(element)
+
+    def pop(self):
+        element = self.peek()
+        self.state.pop()
+        return element
+
+    def peek(self):
+        return self.state[-1]
+
+    def size(self):
+        return len(self.state)
+
+    def __str__(self):
+        str_v = '['
+
+        for elem in self.state:
+            str_v += str(elem) + ','
+
+        if (len(self.state)):
+            str_v = str_v[:-1]
+
+        str_v += ']'
+
+        return str_v
 
 class Grammar:
     def __init__(self, strg_value):
@@ -20,112 +56,111 @@ class Grammar:
         self.productions = split_lines
 
     def get_expansions(self, nonterminal):
-        assert nonterminal[0].isupper()
+        ret_value = Stack()
 
-        return [x[1] for x in self.productions if x[0] == nonterminal]
+        if nonterminal[0].islower():
+            return ret_value
+
+        for x in [y for y in self.productions if y[0] == nonterminal]:
+            ret_value.push(x[1])
+
+        return ret_value
 
 
 class RecursiveDescentRecognizer:
     def __init__(self, grammar):
         self.grammar = grammar
-        self.expansions = {}
-        self.parse_stack = []
+        self.expansions = Stack()
+        self.parse_stack = Stack()
 
-    def register_expansions_for_stack_size(self, stack_size, expansions):
-        self.expansions[stack_size] = expansions
+    def expand_top_symbol(self, tokens_idx):
+        top_symbol = self.parse_stack.peek()['symbol']
 
-    def get_expansions_for_stack_size(self):
-        stack_size = len(self.parse_stack)
-        if not len(self.expansions[stack_size]):
-            return None
-        to_return = self.expansions[stack_size][0]
-        self.expansions[stack_size] = self.expansions[stack_size][1:]
+        if self.expansions.isEmpty() or self.expansions.peek().isEmpty():
+            return False
 
-        return to_return
+        self.parse_stack.peek()['visited'] = True
 
-    def expand_stack(self, expansion):
-        init_size = len(self.parse_stack)
-        self.parse_stack = self.parse_stack[:-1]
-        for i in range(len(expansion)-1, -1, -1):
-            self.parse_stack.append({
-                'symbol': expansion[i],
-                'bpoint': init_size
+        next_expansion = self.expansions.peek().pop()
+
+        backtrack_point = len(next_expansion)
+
+        for i, symbol in enumerate(reversed(next_expansion)):
+            self.parse_stack.push({
+                'symbol': symbol,
+                'backtrack_to': i+1,
+                'tokens_idx': tokens_idx,
+                'visited': False
             })
 
-            expansions = self.grammar.get_expansions(expansion[i]) if not self.is_terminal() else []
-
-            self.register_expansions_for_stack_size(
-                len(self.parse_stack),
-                expansions
+            self.expansions.push(
+                self.grammar.get_expansions(symbol)
             )
 
-    def backtrack(self):
-        print('backtrack called')
-        bpoint = self.parse_stack[-1]["bpoint"]
-
-        self.parse_stack = self.parse_stack[:bpoint-1]
+        return True
 
     def match(self, symbol):
-        if self.parse_stack[-1]['symbol'][0].islower():
-            if self.parse_stack[-1]['symbol'] == symbol:
-                self.parse_stack.pop()
-                return True
-        return False
+        current_symbol = self.parse_stack.peek()['symbol']
 
-    def is_terminal(self):
-        if not len(self.parse_stack):
-            return False
-        return self.parse_stack[-1]['symbol'][0].islower()
+        if current_symbol[0].isupper():
+            return 'nonterminal'
+
+        return 'match' if current_symbol == symbol else 'no match'
+
+    def backtrack(self):
+        n_elems_to_pop = self.parse_stack.peek()['backtrack_to']
+        token_idx = self.parse_stack.peek()['tokens_idx']
+
+        for i in range(n_elems_to_pop):
+            self.parse_stack.pop()
+            self.expansions.pop()
+
+        return token_idx
 
     def parse(self, string):
-        self.parse_stack = []
-        self.expansions = {}
-
         tokens = string.strip().split()
+
         token_idx = 0
 
-        self.register_expansions_for_stack_size(
-            0,
+        self.parse_stack.push({
+            'symbol': self.grammar.start_symbol,
+            'backtrack_to': 1,
+            'visited': False,
+            'tokens_idx': 0
+        })
+
+        self.expansions.push(
             self.grammar.get_expansions(self.grammar.start_symbol)
         )
 
-        self.expand_stack(
-            self.get_expansions_for_stack_size()
-        )
+        self.expand_top_symbol(token_idx)
 
         while token_idx < len(tokens):
-            print('>> Token IDX is {} and parse_stack is \n{}\nexpansions are \n{}\n'.format(token_idx, self.parse_stack, self.expansions))
-            if self.is_terminal():
-                if self.match(tokens[token_idx]):
-                    token_idx += 1
-                else:
-                    print('>Expansions are now {}'.format(self.expansions))
-                    self.backtrack()
+            if self.parse_stack.isEmpty():
+                return False
+
+            match_try = self.match(tokens[token_idx])
+
+            if match_try == 'nonterminal':
+                if not self.expand_top_symbol(token_idx):
+                    token_idx = self.backtrack()
+            elif match_try == 'no match':
+                token_idx = self.backtrack()
             else:
-                next_expansions = self.get_expansions_for_stack_size()
-                print('Expansions are now {}'.format(self.expansions))
+                token_idx += 1
+                self.parse_stack.pop()
+                self.expansions.pop()
 
-                if not next_expansions:
-                    if not len(self.parse_stack):
-                        return False
-                    self.backtrack()
-                    continue
-                else:
-                    self.expand_stack(next_expansions)
+                while not self.parse_stack.isEmpty() and self.parse_stack.peek()['visited']:
+                    self.parse_stack.pop()
+                    self.expansions.pop()
 
-        print('The final parse stack is {}'.format(self.parse_stack))
-
-        return len(self.parse_stack) == 0
-
-
-
-
-
+        return self.parse_stack.isEmpty()
 
 if __name__ == '__main__':
     gr = Grammar(simple_grammar)
 
     pr = RecursiveDescentRecognizer(gr)
 
-    print('The Man Eats is{} a valid phrase'.format(
-        '' if pr.parse("the man eates") else ' not'))
+    print(pr.parse('abracadabra'))
+
